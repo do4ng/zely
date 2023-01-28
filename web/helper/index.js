1510c83527e1aef669d3bd4d8ed3059ad47affc7
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-01-29 00:21:34 +0900

    chore: improved build speed

--- web/helper/index.js
+++ web/helper/index.js
@@ -4,8 +4,7 @@ const { join } = require('path');
 const { getHighlighter } = require('shiki');
 const markdown = require('markdown-it');
 
-async function parseMarkdown(raw) {
-  const highlighter = await getHighlighter({ theme: 'material-palenight' });
+async function parseMarkdown(highlighter, raw) {
   const md = new markdown({
     html: true,
     highlight: (code, lang) => highlighter.codeToHtml(code, { lang }),
@@ -21,8 +20,12 @@ const posts = fs.readdirSync(join(__dirname, '../docs/guide'));
 const result = {};
 
 async function main() {
+  const highlighter = await getHighlighter({ theme: 'material-palenight' });
   for await (const post of posts) {
-    const parsed = await parseMarkdown(fs.readFileSync(join(__dirname, '../docs/guide', post)).toString());
+    const parsed = await parseMarkdown(
+      highlighter,
+      fs.readFileSync(join(__dirname, '../docs/guide', post)).toString()
+    );
 
     result[post] = parsed;
   }
