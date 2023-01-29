bd1cb998dc3d66796ff030032c8f675927a213e2
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-01-29 13:46:18 +0900

    fix: edit typescript middleware example

--- packages/create-prext/assets/with-typescript/pages/index.ts
+++ packages/create-prext/assets/with-typescript/pages/index.ts
@@ -1,5 +1,5 @@
 import { PrextRequest, PrextResponse } from 'prext';
 
 export function get(req: PrextRequest, res: PrextResponse) {
-  res.json({ message: (res as any).message }); // middlewares/message.ts
+  res.json({ message: (req as any).message }); // middlewares/message.ts
 }
