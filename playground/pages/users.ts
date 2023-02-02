c498bf71bc0fea340b2ab53c0629270bb1f36e28
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-02-02 17:52:18 +0900

    [playground] edit example

--- playground/pages/users.ts
+++ playground/pages/users.ts
@@ -4,7 +4,7 @@ export function get(req: PrextRequest, res: PrextResponse) {
   res.setHeader('Content-Type', 'text/html');
   res.end(`
   <div class="users">
-    <h1>User List</h1>
+    <h1>User List - ${(req as any).message}</h1>
     <a href="/users/user1">User 1</a>
     <a href="/users/user2">User 2</a>
     <a href="/users/user3">User 3</a>
--- playground/prext.config.ts
+++ playground/prext.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'prext';
+import { defineConfig } from 'prext/config';
 import { Message } from './middlewares/message';
 
 export default defineConfig({
