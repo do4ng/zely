d16c1766040bcd06f2c1d76d9d91128085c5fe21
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-02-24 10:56:06 +0900

    chore(deps): markdown-it-container

--- web/helper/index.js
+++ web/helper/index.js
@@ -5,17 +5,7 @@ const { getHighlighter } = require('shiki');
 const markdown = require('markdown-it');
 const anchor = require('markdown-it-anchor');
 
-async function parseMarkdown(highlighter, raw) {
-  const md = new markdown({
-    html: true,
-    highlight: (code, lang) => highlighter.codeToHtml(code, { lang }),
-  });
-
-  md.use(anchor.default, {
-    permalink: anchor.default.permalink.ariaHidden({ placement: 'before' }),
-  });
-  md.use(require('markdown-it-header-sections'));
-
+async function parseMarkdown(md, raw) {
   return {
     html: md.render(raw),
   };
@@ -27,12 +17,36 @@ const prextyPosts = fs.readdirSync(join(__dirname, '../docs/prexty'));
 let result = {};
 
 async function main() {
+  // material-palenight
+  // material-lighter
+  // min-light
   const highlighter = await getHighlighter({ theme: 'material-palenight' });
+  const md = new markdown({
+    html: true,
+    highlight: (code, lang) => highlighter.codeToHtml(code, { lang }),
+  });
+
+  md.use(require('markdown-it-container'), 'classname', {
+    validate: (name) => name.trim().length,
+    render: (tokens, idx) => {
+      if (tokens[idx].nesting === 1) {
+        return `<div class="block ${tokens[idx].info.trim()}">\n`;
+      }
+      return '</div>\n';
+    },
+  });
+
+  md.use(anchor.default, {
+    permalink: anchor.default.permalink.ariaHidden({ placement: 'before' }),
+  });
+  md.use(require('markdown-it-header-sections'));
+
+  // parser
 
   for await (const post of posts) {
     console.log(`Parsing ${post}`);
     const parsed = await parseMarkdown(
-      highlighter,
+      md,
       fs.readFileSync(join(__dirname, '../docs/guide', post)).toString()
     );
 
@@ -46,7 +60,7 @@ async function main() {
   for await (const post of prextyPosts) {
     console.log(`Parsing ${post}`);
     const parsed = await parseMarkdown(
-      highlighter,
+      md,
       fs.readFileSync(join(__dirname, '../docs/prexty', post)).toString()
     );
 
